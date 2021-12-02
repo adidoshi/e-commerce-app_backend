aa6bd4434771e0993f378dba90f99818d58ebf98
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2021-12-02 19:45:27 +0530

    fix : cors credentials error

--- server.js
+++ server.js
@@ -21,7 +21,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.json({
