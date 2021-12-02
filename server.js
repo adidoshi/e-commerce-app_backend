75508cb490f327f455e27ca1ef299e5aaf5f88b1
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2021-12-02 17:43:52 +0530

    fix: cors package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config/config.env" });
 const express = require("express");
+const cors = require("cors");
 const cloudinary = require("cloudinary");
 const fileUpload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({
