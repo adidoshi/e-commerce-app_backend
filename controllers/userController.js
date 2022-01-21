fd3d8a2d915e36888e1598237668f88c8e37a720
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2022-01-22 01:59:31 +0530

    fix: cloudinary upload

--- controllers/userController.js
+++ controllers/userController.js
@@ -190,9 +190,9 @@ exports.updateProfile = catchAsyncError(async (req, res) => {
   if (req.body.avatar !== "") {
     const user = await User.findById(req.user.id);
 
-    const imageId = user.avatar.public_id;
+    // const imageId = user.avatar.public_id;
 
-    await cloudinary.v2.uploader.destroy(imageId);
+    // await cloudinary.v2.uploader.destroy(imageId);
 
     const myCloud = await cloudinary.v2.uploader.upload(req.body.avatar, {
       folder: "avatars",
