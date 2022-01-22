66cdbbd39599c56a1f3720bd662544f78cddc331
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2022-01-22 15:56:50 +0530

    feat: razorpay payment instance

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,33 @@
 const catchAsyncError = require("../middleware/catchAsyncError");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const shortid = require("shortid");
+const Razorpay = require("razorpay");
+
+const instance = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+exports.razorpayPayment = catchAsyncError(async (req, res, next) => {
+  const options = {
+    amount: req.body.amount,
+    currency: "INR",
+    receipt: shortid.generate(),
+  };
+
+  try {
+    const response = await instance.orders.create(options);
+    res.status(200).json({
+      success: true,
+      details: response,
+    });
+  } catch (error) {
+    res.status(400).json(`Error occured -${error}`);
+    console.log(error);
+  }
+});
+
 exports.processPayment = catchAsyncError(async (req, res, next) => {
   const myPayment = await stripe.paymentIntents.create({
     amount: req.body.amount,
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,11 +2,14 @@ const express = require("express");
 const {
   processPayment,
   sendStripeApiKey,
+  razorpayPayment,
 } = require("../controllers/paymentController");
 const router = express.Router();
 const { isAuthenticatedUser } = require("../middleware/auth");
 
 router.route("/payment/process").post(isAuthenticatedUser, processPayment);
 router.route("/stripeApiKey").get(isAuthenticatedUser, sendStripeApiKey);
+
+router.route("/payment/razorpay").post(isAuthenticatedUser, razorpayPayment);
 
 module.exports = router;
